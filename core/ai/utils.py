039c800a2a34ab3e5644fcb0d00b82a1f6e577cc
keran-w <wangk16@rpi.edu>
2022-12-03 22:37:23 -0500

    Update utils.py

--- core/ai/utils.py
+++ core/ai/utils.py
@@ -23,3 +23,22 @@ def label_enc(data_owner_dataset):
     labels = le.transform(data_owner_dataset.label_name)
     images = data_owner_dataset.image.tolist()
     return images, labels, label2id, id2label
+
+# Get evaluation metrics (accuracy, f1 score, and the weighted ones)
+def get_metrics(labels, predictions, label2id):
+    from sklearn import metrics
+    # Get accuracy
+    accuracy_score = metrics.accuracy_score(labels, predictions)
+
+    # Get f1 score
+    f1_score = metrics.f1_score(labels, predictions, average='micro')
+
+    # Get weighted accuracy
+    sample_weight = [5 if label==label2id['other'] else 1 for label in labels]
+    weighted_accuracy_score = metrics.accuracy_score(labels, predictions, sample_weight=sample_weight)
+
+    # Get weighted f1 score
+    weighted_f1_score = metrics.f1_score(labels, predictions, average='micro', sample_weight=sample_weight)
+    
+    return accuracy_score, f1_score, weighted_accuracy_score, weighted_f1_score
+
