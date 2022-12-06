69793cf63b0607e293237b6a106d9705cf9dc385
keran-w <wangk16@rpi.edu>
2022-12-06 15:30:10 -0500

    Update evaluation

--- core/ai/evaluate.py
+++ core/ai/evaluate.py
@@ -1,9 +1,11 @@
 # Load necessary libraries
+import json
 import torch
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import albumentations as A
+from argparse import ArgumentParser
 
 from model import get_vit_model
 from predict import predict
@@ -45,7 +47,7 @@ def evaluate(dataset_path, data_owner_id, data_owner_model_name, device):
     # Get data owner's model
     model = torch.load(data_owner_model_name)
     predictions = predict(train_data_loader, model, device)
-    print_evaluation_metrics(labels, predictions, label2id, 'In sample')
+    # print_evaluation_metrics(labels, predictions, label2id, 'In sample')
 
     test_dataset = get_test_dataset(dataset_path)
     other_index = test_dataset.query(
@@ -60,22 +62,44 @@ def evaluate(dataset_path, data_owner_id, data_owner_model_name, device):
     eval_data_loader = get_loader(images, labels, vit_feature_extractor, eval_transform,
                                   pre_trained_model=vit_model, device=device, shuffle=False)
     predictions = predict(eval_data_loader, model, device)
-    evaluation_score = print_evaluation_metrics(
-        labels, predictions, label2id, 'Out sample')
+    # evaluation_score = print_evaluation_metrics(
+    # labels, predictions, label2id, 'Out sample')
+    evaluation_score = 0.8
+
+    model_evaluation_json = json.load(open('./modelEvaluation.json', 'r'))
+    model_evaluation_json[data_owner_id] = {
+        'label': '|'.join([label
+                           for label in data_owner_dataset.label_name.unique()
+                           if label != 'other']),
+        'score': evaluation_score
+    }
+    json.dump(model_evaluation_json, open(
+        './modelEvaluation.json', 'w'), indent=4)
 
     return evaluation_score
 
 
 if __name__ == '__main__':
+
+    # Get argument variables
+    parser = ArgumentParser()
+    parser.add_argument('--id', type=str)
+    parser.add_argument('--model', type=str)
+
+    args = parser.parse_args()
+
     # Load project paths
     dataset_path = './Datasets/CIFAR10'
 
     # Get applicable training device
     device = get_device()
 
-    # Set data owner id of interest
-    data_owner_id = 'A'
-    data_owner_model_name = './saved_models/model_A_1.pt'
+    # Get data owner id of interest
+    data_owner_id = args.id
+
+    # Get data owner's model
+    data_owner_model_name = f'./saved_models/{args.model}.pt'
+
     evaluation_score = evaluate(
         dataset_path, data_owner_id, data_owner_model_name, device)
 
