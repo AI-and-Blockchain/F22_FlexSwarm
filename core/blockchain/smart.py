1e25aaf7d6691939cf05a7e0925a00a3e0971d89
kqin <qinz@rpi.edu>
2022-12-08 09:10:18 -0500

    Simplified back end and integration test

--- core/blockchain/smart.py
+++ core/blockchain/smart.py
@@ -76,28 +76,30 @@ def createTxn(algoclient, owner_address, owner_private, client_address, client_p
 # generate_algorand_keypair("A")
 # generate_algorand_keypair("B")
 
-issuers = 'X27FRTBPN2WOC4M3G3Q3TLRYNJTBQO7JALAIVF36XZ646GJD3R3PFUGWDU' # For reward 
+if __name__ == '__main__':
 
-owner_address = 'B3HOVTUXTGSPOZXUAMUDUZ3DXJYDIDSIPKQZVPGBPUKV6HMNIUYJ7SDPDQ'
-owner_passphrase = 'badge chicken forest decide day misery canvas huge decorate fever food current shy camp black coyote elder another reward soap knee slush property about rug'    
+    issuers = 'X27FRTBPN2WOC4M3G3Q3TLRYNJTBQO7JALAIVF36XZ646GJD3R3PFUGWDU' # For reward 
 
-
-client_address = 'KHCZWNHJE464SNFAS76LZYAXD4ZDIR5M5VB73YWLDQHHMZ3HQPCEAWCZMA'
-client_passphrase = 'bar sheriff long setup world vacant ski essence web proof arrow grant parrot grocery visit spawn reward stool correct cake asset clap accuse absent junk'
-client_privateKey = mnemonic.to_private_key(client_passphrase)
+    owner_address = 'B3HOVTUXTGSPOZXUAMUDUZ3DXJYDIDSIPKQZVPGBPUKV6HMNIUYJ7SDPDQ'
+    owner_passphrase = 'badge chicken forest decide day misery canvas huge decorate fever food current shy camp black coyote elder another reward soap knee slush property about rug'    
 
 
-# Connect to the algod client
-purestake_key = 'pHY0FnbMby3VKnaN0RkT03CIsG0vacUp33jPvvOk'
-algod_address = 'https://testnet-algorand.api.purestake.io/ps2'
-algod_token = "pHY0FnbMby3VKnaN0RkT03CIsG0vacUp33jPvvOk"
-headers = {
-    'X-Api-key': "B3SU4KcVKi94Jap2VXkK83xx38bsv95K5UZm2lab",
-}
-
-algoclient = algod.AlgodClient(algod_token, algod_address, headers)
+    client_address = 'KHCZWNHJE464SNFAS76LZYAXD4ZDIR5M5VB73YWLDQHHMZ3HQPCEAWCZMA'
+    client_passphrase = 'bar sheriff long setup world vacant ski essence web proof arrow grant parrot grocery visit spawn reward stool correct cake asset clap accuse absent junk'
+    client_privateKey = mnemonic.to_private_key(client_passphrase)
 
 
+    # Connect to the algod client
+    purestake_key = 'pHY0FnbMby3VKnaN0RkT03CIsG0vacUp33jPvvOk'
+    algod_address = 'https://testnet-algorand.api.purestake.io/ps2'
+    algod_token = "pHY0FnbMby3VKnaN0RkT03CIsG0vacUp33jPvvOk"
+    headers = {
+        'X-Api-key': "B3SU4KcVKi94Jap2VXkK83xx38bsv95K5UZm2lab",
+    }
 
-createTxn(algoclient, owner_address, mnemonic.to_private_key(owner_passphrase), client_address, client_privateKey)
+    algoclient = algod.AlgodClient(algod_token, algod_address, headers)
+
+
+
+    createTxn(algoclient, owner_address, mnemonic.to_private_key(owner_passphrase), client_address, client_privateKey)
 
--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,31 +1,74 @@
 // Account creation / Sign in by wallet connect
-import React, {useState, useContext, Suspense} from 'react';
+import React, {useState, useContext, useEffect, Suspense} from 'react';
 import { useNavigate } from 'react-router-dom';
 import "./Home.css";
 import 'bootstrap/dist/css/bootstrap.css';
 import axios from 'axios';
+import { Button } from 'react-bootstrap';
 
 
 const Home = () => {
   
   let navigate = useNavigate();
-  const [PubKey, setPubKey] = useState(false);
+  const [pubKey, setPubKey] = useState();
+  const [login, setLogin] = useState(false);
+  const [testRes, setTestRes] = useState();
 
   // connect with public key /or mneumonics
-  const loginCheck = async () => {
-    // const res = await connect
-    return 1;
-  }
+  // const loginCheck = async () => {
+  //   // const res = await connect
+  //   return 1;
+  // };
 
-  const createAccount = async () => {
-    // const data = await axios.get('/account');
-    return 1;
-  }
+  const createAccount = async (name) => {
+    await axios.post('http://localhost:8888/generateAcnt', {'params': {'name': name}})
+    .then( res => {
+      setTestRes(JSON.stringify(res.data));
+      console.log(JSON.stringify(res.data));
+    })
+    .catch( (err) => {
+      console.error(err);
+    })
+  };
 
   const test = async () => {
-    // await axios.get()
-    return 1;
-  }
+    await axios.get('http://localhost:8888')
+    .then( res => {
+      setTestRes(res.data.data);
+    })
+    .catch( (err) => {
+      console.error(err);
+    })
+  };
 
+  useEffect( () => {
+    test();
+  }, []);
+
+  return (
+    <div id='home' className='content content-center'>
+      
+      <div id="login">
+        <button variant="primary" onClick={ () => {navigate('/modelupload');}}>Model Upload</button>
+        <button variant="primary" onClick={ async () => {navigate('/modelrun');}}>Model Run</button>
+        <button variant="primary" onClick={ async () => { await createAccount('Test')}}>Test Generate Account</button>
+      </div>
+
+      {
+        test ? 
+        <>
+          <h3>Test Response</h3>
+          <div>{testRes}</div>
+        </>
+        :
+        <>
+        <div>No results found</div>
+        </>
+      }
+    </div>
+    
+  )
   //
 }
+
+export default Home;
